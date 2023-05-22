2fefc79dd686251bffb66dfddc2ee469a1f3073b
Lucy <lucybellott@gmail.com>
2023-05-22 12:28:36 -0400

    getting api data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
 
 
-const apiKey = 'd38b20925679d2d417fddef4f8cb381a'
-
 function App() {
+  
+
+
+  const apiKey = 'd38b20925679d2d417fddef4f8cb381a'
+
+
+const [weather, setWeather] = useState({})
+
+  useEffect(() => {
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${apiKey}
+    `)
+    .then(resp => resp.json())
+    .then(data => {
+          console.log(data)
+    })
+    }, [])
+  
+  
+  
+  
+  
+  
+  
+  
+  
   return (
     <div className="App">
     <h1>Meme Weather</h1>
